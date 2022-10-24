82f947d352eab0a6ad6c5747ecdad915abba08ff
Paulfresnel <paul.fresnel1@gmail.com>
2022-10-24 23:47:20 +0200

    small

---
+++ script.js
@@ -0,0 +1,9 @@
+let header = document.getElementById('header');
+
+let main = document.getElementById('main');
+
+window.addEventListener('load',()=>{
+    header.style.width = main.style.width;
+})
+
+
